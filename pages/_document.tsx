65da0f3d34a419aff68d9de45d2bff30baed4eb6
Jun Otani <jun-otani@kcvn.com.vn>
2021-02-25 17:58:46 +0700

    add new deps

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head />
-        <body className="bg-blue-200">
+        <body className="dark: bg-gray-700 font-sans">
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
-import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
-import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
-import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
-import Post from "../types/post";
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Post from '../types/post'
 
 type Props = {
-  allPosts: Post[];
-};
+  allPosts: Post[]
+}
 
 const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
@@ -23,7 +21,6 @@ const Index = ({ allPosts }: Props) => {
           {heroPost && (
             <HeroPost
               title={heroPost.title}
-              coverImage={heroPost.coverImage}
               date={heroPost.date}
               author={heroPost.author}
               slug={heroPost.slug}
@@ -34,22 +31,15 @@ const Index = ({ allPosts }: Props) => {
         </Container>
       </Layout>
     </>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
 
 export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
+  const allPosts = getAllPosts(['title', 'date', 'slug', 'author', 'coverImage', 'excerpt'])
 
   return {
-    props: { allPosts },
-  };
-};
+    props: { allPosts }
+  }
+}
